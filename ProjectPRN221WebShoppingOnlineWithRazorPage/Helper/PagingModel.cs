215496d6ae5c289f33c8a206b5277904fa2a8cfb
DuyKhanh <caankhwh1303@gmail.com>
2023-10-24 09:55:09 +0700

    Add project files.

---
+++ ProjectPRN221WebShoppingOnlineWithRazorPage/Helper/PagingModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProjectPRN221WebShoppingOnlineWithRazorPage.Helper
+{
+    public class PagingModel
+    {
+        // trang hiện tại
+        public int currentpage { get; set; }    
+        // tổng số trang
+        public int countpages { get; set; }
+        // nhận 1 tham số int và trả về một url
+        public Func<int?, string> generateUrl { get; set; } 
+    }
+}
