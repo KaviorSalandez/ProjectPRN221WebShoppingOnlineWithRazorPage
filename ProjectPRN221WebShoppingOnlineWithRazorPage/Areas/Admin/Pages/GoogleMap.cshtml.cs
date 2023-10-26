6681519bbde1e7d58831c5f908702a56d394ce85
DuyKhanh <caankhwh1303@gmail.com>
2023-10-26 16:34:27 +0700

    nhúng map cho trang admin

---
+++ ProjectPRN221WebShoppingOnlineWithRazorPage/Areas/Admin/Pages/GoogleMap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectPRN221WebShoppingOnlineWithRazorPage.Areas.Admin.Pages
+{
+    public class GoogleMapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
